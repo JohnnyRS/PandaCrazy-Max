79af5cc8cceed2e6063d02396365665db5d5da9e
John Ramirez <johnnyrs@allbyjohn.com>
2019-12-22 14:24:08 -0800

    Timer Class is now in background

--- js/classes/TimerClass.js
+++ js/classes/TimerClass.js
@@ -1,25 +1,41 @@
 class TimerClass {
-	constructor(timer = null) {
+	constructor(timer = null, hamTimeout = null) {
 		this.timeout = timer;
+		this.hamTimeout = hamTimeout;
+		this.timeoutID = null;
+		this.timeoutDoing = null;
 		this.queue = [];
 		this.queueObject = [];
 		this.queueJumping = [];
 		this.queueSkipped = [];
 		this.unique = 1;
-		this.paused = false;
-		this.running = false;
-		this.goingHam = null;
+		this._running = false;
+		this._goingHam = null;
+		this._paused = false;
 		this.started = null; // time started
+		this.port = null;
 	}
+	get running() { return this._running; }
+	set running(v) { this._running = v; this.sendToPort(); }
+	get goingHam() { return this._goingHam; }
+	set goingHam(v) { this._goingHam = v; this.sendToPort(); }
+	get paused() { return this._paused; }
+	set paused(v) { this._paused = v; this.sendToPort(); }
+	sendToPort() { if (this.port) { this.port.postMessage({return:"timerInfo", running:this._running, goingHam:this._goingHam, paused:this._paused, queueTotal:Object.keys(this.queueObject).length, myIdHam:(this._goingHam!==null) ? this.queueObject[this._goingHam].myId : -1}); }
+		}
+	setPort(port) { this.port = port; }
 	setTimer(timer) { this.timeout = timer; }
+	setHamTimer(timer) { this.hamTimeout = timer; }
 	returnTimer() { return this.timeout; }
 	privateLoop() {
+		this.timeoutDoing = this.timeoutID; this.timeoutID = null;
 		const end = new Date().getTime(); let goodToGo = true, queueUnique = null, turnOffHam = false;
+		const usingTimer = (this.goingHam!==null && this.hamTimeout!==null) ? this.hamTimeout : this.timeout;
 		const elapsed = end - this.started;
-		const diff = ((elapsed - this.timeout) > 0) ? elapsed - this.timeout : 0;
-		let newTimer = this.timeout - diff;
-		if (this.timeout !== null && !this.paused && this.queue.length > 0) {
-			if (this.goingHam!==null) queueUnique = this.goingHam;
+		const diff = ((elapsed - usingTimer) > 0) ? elapsed - usingTimer : 0;
+		let newTimer = usingTimer - diff;
+		if (usingTimer!==null && !this.paused && this.queue.length > 0) {
+			if (this.goingHam!==null) { queueUnique = this.goingHam; }
 			else queueUnique = this.queue.shift();
 			const thisItem = this.queueObject[queueUnique];
 			if (thisItem) {
@@ -30,68 +46,147 @@ class TimerClass {
 				}
 				if (thisItem.tGoHam!==-1) {
 					if (thisItem.hamstarted===null) thisItem.hamstarted = new Date().getTime();
-					else if ( (end - thisItem.hamstarted) > thisItem.tGoHam ) { turnOffHam=true; thisItem.tGoHam = -1; thisItem.hamstarted = null; enableAllHamButtons(); }
+					else if ( (end - thisItem.hamstarted) > thisItem.tGoHam ) { turnOffHam=true; thisItem.tGoHam = -1; thisItem.hamstarted = null; }
 				}
 				if (goodToGo) {
 					thisItem.theFunction.apply(this, [queueUnique, elapsed, thisItem.myId, thisItem.obj]);
 					this.started = new Date().getTime();
-					this.running = true; }
+				}
 				else if (!goodToGo) {
-					this.deleteFromQueue(queueUnique);
 					thisItem.funcAfter.apply(this, [thisItem.myId, thisItem.obj, turnOffHam]);
+					this.deleteFromQueue(queueUnique);
 					if (this.goingHam===queueUnique) this.goingHam = null;
 					newTimer = 0;			// Pass to next in queue
 				}
 				if (goodToGo && this.goingHam===null) this.queue.push(queueUnique);
-				if (turnOffHam) this.hamOff(queueUnique);
-				this.timerObj = setTimeout(this.privateLoop.bind(this), newTimer);
-				// if (!goodToGo) console.log(`Leaving ${queueUnique} on the floor`);
+				if (turnOffHam) this.hamOff();
+				this.timeoutDoing = null;
+				this.timeoutID = setTimeout(this.privateLoop.bind(this), newTimer);
 			} else { console.log(`Caught that little buggy: ${queueUnique} | ${this.queue} | ${this.goingHam}`); }
 		} else { this.running = false; }
 	}
 	goTimer() { // start timer if it's not running
 		if (!this.running && this.timeout !== null && !this.paused && this.queue.length > 0) {
 			this.running = true;
-			this.started = new Date().getTime();
-			this.timerObj = setTimeout(this.privateLoop.bind(this), 0);
+			this.started = new Date().getTime(); this.tempStarted = new Date().getTime();
+			this.timeoutID = setTimeout(this.privateLoop.bind(this), 0);
 		}
 	}
 	removeFromQueue(queueUnique) { this.queue = arrayRemove(this.queue, queueUnique); console.log(`queueUnique: ${queueUnique} is removed - ${this.queue}`); }
 	removeFromQueueSkipped(queueUnique) { this.queueSkipped = arrayRemove(this.queueSkipped, queueUnique); }
-	goHam(queueUnique, tGoHam=null) { if (this.goingHam===null) { this.goingHam=queueUnique; if (tGoHam!==null) this.queueObject[queueUnique].tGoHam = tGoHam; } }
-	hamOff(queueUnique) { if (this.goingHam===queueUnique) this.goingHam=null; }
+	adjustTimer(newTimer) {
+		if (this.timeoutID!==null && this.timeoutDoing!==this.timeoutID) {
+			clearTimeout(this.timeoutID);
+			const timeElapsed = newTimer - (new Date().getTime() - this.started), newTimeout = (timeElapsed > 0) ? timeElapsed : 0;
+			this.timeoutDoing = null; this.timeoutID = setTimeout(this.privateLoop.bind(this), newTimeout);
+		}
+	}
+	goHam(queueUnique, tGoHam=null) {
+		if (this.goingHam===null) {
+			if (tGoHam!==null) this.queueObject[queueUnique].tGoHam = tGoHam;
+			this.goingHam=queueUnique; this.adjustTimer(this.hamTimeout);
+		}
+	}
+	hamOff(queueUnique) { this.goingHam = null; this.adjustTimer(this.timeout); if (queueUnique in this.queueObject) this.queueObject[queueUnique].tGoHam = null; }
 	isHamOn() { return this.goingHam!==null; }
 	isGoingHam(queueUnique) { return this.goingHam===queueUnique; }
+	isRunning() { return this.running; }
 	runQueueTimer() { this.goTimer(); }
 	pauseTimer() { this.paused = true; }
 	unPauseTimer() { this.paused = false; this.goTimer(); }
-	skipThis(queueUnique) { console.log("Trying to skip");
-		if (queueUnique in this.queueObject) {
-			this.removeFromQueue(queueUnique);
-			this.queueObject[thisUnique].skipped = true;
+	pauseToggle() { this.paused = !this.paused; this.goTimer(); return this.paused;  }
+	stopAll() {
+		if (this.queue.length) {
+			this.queue.forEach( (queueUnique) => {
+				const thisItem = this.queueObject[queueUnique];
+				thisItem.funcAfter.apply(this, [thisItem.myId, thisItem.obj, false]);
+				this.deleteFromQueue(queueUnique);
+			});
+			this.goingHam = null; this.running = false; this.queue = [];
 		}
 	}
-	unSkipThis(queueUnique) {
-		if (queueUnique in this.queueSkipped) {
+	skipThis(queueUnique) { console.log("Trying to skip");
+		if (queueUnique in this.queueObject) {
+			this.removeFromQueue(queueUnique); this.queueSkipped.push(queueUnique);
+			if (this.goingHam=queueUnique) this.hamOff();
+			this.queueObject[queueUnique].skipped = true;
+			console.log(this.queue,this.queueSkipped);
+		}
+	}
+	unSkipThis(queueUnique) { console.log("trying to unskip: ", queueUnique,this.queueSkipped);
+		if (this.queueSkipped.includes(queueUnique)) {
 			this.removeFromQueueSkipped(queueUnique);
-			this.queue.unshift(queueUnique); console.log(this.queue);
+			this.queueObject[queueUnique].skipped = false;
+			this.queue.unshift(queueUnique); console.log(this.queue,this.queueSkipped);
+			this.goTimer();
 		}
 	}
 	resetTimeStarted(queueUnique) {
 		if (queueUnique in this.queueObject && ( this.queueObject[queueUnique].tDuration!==-1 || this.queueObject[queueUnique].duration!==-1) ) this.queueObject[queueUnique].timeStarted=null;
 	}
-	addToQueue(myId, thisObj, func, funcAfter, duration=-1, tempDuration=-1, tempGoHam=-1) {
+	addToQueue(myId, thisObj, func, funcAfter, goHamStart=false, duration=-1, tempDuration=-1, tempGoHam=-1) {
 		const thisUnique = this.unique++;
 		this.queue.unshift(thisUnique);
 		tempDuration = (duration!==-1 && tempDuration>duration) ? -1 : tempDuration;
 		this.queueObject[thisUnique] = { theFunction:func, funcAfter:funcAfter, myId:myId, obj:thisObj, duration:duration, tDuration:tempDuration, tGoHam:tempGoHam, timeStarted:null, hamstarted:null, skipped:false };
-		if (tempGoHam!==-1 && this.goingHam===null) { this.goHam(thisUnique); disableOtherHamButtons(myId); }
+		if ( (tempGoHam!==-1 || goHamStart) && this.goingHam===null) { this.goHam(thisUnique); }
 		this.goTimer();
+		this.sendToPort();
 		return thisUnique;
 	}
 	deleteFromQueue(queueUnique) {
+		if (this.goingHam===queueUnique) this.goingHam = null;
 		this.removeFromQueue(queueUnique);
 		delete this.queueObject[queueUnique];
-		if (this.goingHam===queueUnique) this.goingHam = null;
+		if (this.queue.length===0) this.running = false;
+		this.sendToPort();
 	}
+}
+
+const pandaTimer = new TimerClass(995,970); // little lower than 1s panda timer by default
+const queueTimer = new TimerClass(2000); // 2s queue monitor by default
+const searchTimer = new TimerClass(950);
+let portsConnected = 0;
+
+if (chrome.runtime) {
+	chrome.runtime.onConnect.addListener( function(port) {
+		let portTimer = null;
+		console.log(`port name: ${port.name} is NOW connected!`);
+		if (port.name==="pandaTimer") { portTimer = pandaTimer; pandaTimer.setPort(port); }
+		else if (port.name==="queueTimer") { portTimer = queueTimer; queueTimer.setPort(port); }
+		else if (port.name==="searchTimer") { portTimer = searchTimer; searchTimer.setPort(port); }
+		if (portTimer) {
+			portsConnected++;
+			port.onMessage.addListener(function(msg) {
+				if (msg.command == "addToQueue") {
+					const value = portTimer.addToQueue(msg.myId, msg.thisObj, (a1, a2, a3, a4) => {
+							if (port) port.postMessage({return:msg.doThis, queueUnique:a1, elapsed:a2, myId:a3, obj:a4});
+						}, (a1, a2, a3) => {
+							if (port) port.postMessage({return:msg.doAfter, myId:a1, obj:a2, turnOffHam:a3});
+						}, msg.goHamStart, msg.duration, msg.tempDuration, msg.tempGoHam);
+					port.postMessage({return:"addToQueue", myId:msg.myId, obj:msg.thisObj, value:value});
+				}
+				else if (msg.command=="setTimer") { portTimer.setTimer(msg.timer, msg.mainTimer); }
+				else if (msg.command=="setHamTimer") { portTimer.setHamTimer(msg.timer); }
+				else if (msg.command=="pauseToggle") { portTimer.pauseToggle(); }
+				else if (msg.command=="pauseTimer") { portTimer.pauseTimer(); }
+				else if (msg.command=="unPauseTimer") { portTimer.unPauseTimer(); }
+				else if (msg.command=="stopAll") { portTimer.stopAll(); }
+				else if (msg.command=="deleteFromQueue") { portTimer.deleteFromQueue(msg.queueUnique); }
+				else if (msg.command=="isGoingHam") {  port.postMessage({return:"isGoingHam", value:portTimer.isGoingHam(msg.queueUnique)}); }
+				else if (msg.command=="isHamOn") { port.postMessage({return:"isHamOn", value:portTimer.isHamOn()}); }
+				else if (msg.command=="isRunning") { port.postMessage({return:"isRunning", value:portTimer.isRunning()}); }
+				else if (msg.command=="goHam") { portTimer.goHam(msg.queueUnique, msg.tGoHam); }
+				else if (msg.command=="hamOff") { portTimer.hamOff(); }
+				else if (msg.command=="unSkipThis") { portTimer.unSkipThis(msg.queueUnique); }
+				else if (msg.command=="skipThis") { portTimer.skipThis(msg.queueUnique); }
+				else if (msg.command=="resetTimeStarted") { portTimer.resetTimeStarted(msg.queueUnique); }
+			});
+			port.onDisconnect.addListener( () => {
+				console.log(`TC port name: ${port.name} is disconnected!`);
+				port = null; portsConnected = (portsConnected>0) ? portsConnected-1 : 0;
+				if (portsConnected===0) queueTimer.stopAll();
+			});
+		}
+	});
 }
--- js/pandaCrazyMax.js
+++ js/pandaCrazyMax.js
@@ -1,52 +1,11 @@
-function createInput(appendHere, divAddClass, id, label, placeholder) {
-  $(`<div class="form-inline w-100${divAddClass}"></div>`).append($(`<label for="${id}" class="col-sm-3 text-right">${label}</label>`)).append($(`<input type="text" class="form-control pcm_inputText-md ml-2 col-sm-7 text-left" id="${id}" placeholder="${placeholder}">`)).appendTo(appendHere);
-}
-function createCheckBox(appendHere, label, id, value, checked) {
-  const checkedText = (checked) ? " checked" : "";
-  const formCheck = $(`<div class="form-check form-check-inline"></div>`).appendTo(appendHere);
-  $(`<input class="form-check-input${checkedText}" type="checkbox" id="${id}" value="${value}"${checkedText}>`).appendTo(formCheck);
-  $(`<label class="form-check-label" for="${id}">${label}</label>`).appendTo(formCheck);
-}
-function saveToFile(theData) {
-  var blob = new Blob( [JSON.stringify(theData)], {type: "text/plain"}), dl = document.createElement("A");
-  dl.href = URL.createObjectURL(blob); dl.download = "PCM_test.json";
-  document.body.appendChild(dl); dl.click(); dl.remove();
-}
-function enableAllHamButtons() {
-  $(".pcm_hamButton").removeClass("disabled").addClass("pcm_buttonOff");
-}
-function disableOtherHamButtons(myId=null) {
-  if (myId!==null) $(`#pcm_hamButton_${myId}`).removeClass("pcm_buttonOff").addClass("pcm_buttonOn");
-  $(".pcm_hamButton.pcm_buttonOff").addClass("disabled");
-}
-function formatAMPM(theFormat,theDate,theTimeZone) {
-  var d = (theDate) ? theDate : new Date();
-  if (theTimeZone === "mturk") {
-    let mturkTZOffset = -8, today = new Date(); if (today.dst()) mturkTZOffset++;
-    let utc = d.getTime() + (d.getTimezoneOffset() * 60000), MturkTime = utc + (3600000 * mturkTZOffset);
-    d = new Date(MturkTime);
-  }
-  let minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
-      hours = d.getHours(), ampm = hours >= 12 ? 'pm' : 'am',
-      months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'], days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-  hours = (hours>= 12) ? (hours-12) : hours;
-  hours = (hours.toString().length === 1) ? '0'+hours : hours;
-  if (theFormat==="short") return ('0' + (d.getMonth()+1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2) + '-' + d.getFullYear() + '(' + hours + ':' + minutes + ampm + ')';
-  else if (theFormat==="dayandtime") return days[d.getDay()] + ' ' + hours + ':' + minutes + ampm;
-  else if (theFormat==="onlydate") return ('0' + (d.getMonth()+1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2) + '-' + d.getFullYear();
-  else return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
-}
-function arrayRemove(arr,value) { return arr.filter( (item) => item !== value ); }
-function shortenGroupId(gId) { return gId.slice(0, 2) + "..." + gId.slice(-4); }
-MturkPanda._init_Timer(995); // initialise a static timer for all panda's
 MturkPanda._init_GStats(); // initialise a static stats for all global panda stats
 
 const panda = new MturkPanda(); // set up a mturk class for a panda
 const modal = new ModalClass(panda); // set up a modal class for a options, warnings or details
-const queue = new MturkQueue(); // set up a mturk class for a panda
 const menus = new MenuClass("pcm_quickMenu", panda); // set up a mturk class for a panda
-const dataShow = new DataShowClass(); // set up a datashow class to display data easier
 const pandaTabs = new TabbedClass($(`#pcm_pandaSection`)); // set up a mturk class for a panda
+const groupings = new PandaGroupings();
+const alarms = new AlarmsClass();
 panda.addTabsObj(pandaTabs);
 
 $(".sortable").sortable({connectWith: ".sortable"}).disableSelection();
@@ -56,18 +15,19 @@ $(".sortable").sortable({connectWith: ".sortable"}).disableSelection();
   panda.addPanda("30B721SJLR5BYYBNQJ0CVKKCWQZ0OI", "Tell us if two receipts ar the same", "Tell us if two receipts ar the same", "AGVV5AWLJY7H2", "Ibotta, Inc.", "0.01", false, 12, 0, true, 4000, -1, 0);
   // **************************************************************
   
-// Usually the main page is faster to load so it probably already got the queue.
-const queueResults = localStorage.getItem("PCM_queueResults");
-if (queueResults) queue.setQueueResults(JSON.parse(queueResults));
-
-window.addEventListener("storage", event => {
-  if (event.key==="PCM_addAndRunPanda") {
-    const v = JSON.parse(event.newValue);
-    panda.addAndRunPanda(v.gId, v.desc, v.title, v.rId, v.reqName, v.price, v.once, v.limitNumQueue, v.limitTotalQueue, v.hitsAvailable, v.tempDuration, v.tempGoHam);
-    localStorage.removeItem("PCM_addAndRunPanda");
-  } else if (event.key==="PCM_queueResults") {
-    const v = JSON.parse(event.newValue);
-    queue.setQueueResults(v);
-    panda.gotNewQueue();
-  }
-}, false);
+  groupings.addGroupings("1 group","My first grouping", [[0,false], [1,false]]);
+  groupings.addGroupings("2 group","My second grouping", [[0,false], [1,false]]);
+  groupings.addGroupings("3 group","My third grouping", [[0,false], [1,false]]);
+  
+  window.addEventListener("storage", event => {
+    if (event.key==="PCM_addAndRunPanda") {
+      const v = JSON.parse(event.newValue);
+      panda.addAndRunPanda(v.gId, v.desc, v.title, v.rId, v.reqName, v.price, v.once, v.limitNumQueue, v.limitTotalQueue, v.hitsAvailable, v.tempDuration, v.tempGoHam);
+      localStorage.removeItem("PCM_addAndRunPanda");
+    } else if (event.key==="PCM_queueResults") {
+      const v = JSON.parse(event.newValue);
+      queue.setQueueResults(v);
+      panda.gotNewQueue();
+    }
+  }, false);
+  
