9fb6ab4e2d4bb97c6a7159c42a7768d4f08502f6
John Ramirez <johnnyrs@allbyjohn.com>
2020-05-07 15:20:34 -0700

    Search Crazy page added

---
+++ js/searchCrazy.js
@@ -0,0 +1,18 @@
+const bgPage = chrome.extension.getBackgroundPage(); // Get background page for extension
+const searchUI = new SearchUI();
+const bgSearchClass = bgPage.gSetSearchUI(searchUI);
+const bgQueueClass = bgPage.gReturnQueueClass();
+const modal = new ModalClass(); // set up a modal class for a options, warnings or details
+
+function startSearchCrazy() {
+  window.addEventListener("beforeunload", (e) => { bgPage.gSetSearchUI(null); bgSearchClass.closedUI(); });
+  searchUI.prepareSearch();
+}
+
+function displayDebuggerError(title,message) {
+  $(".pcm_top:first").html("");
+  console.log(message); $('#pcm_searchTriggers .tab-content').html(`<H1 style="text-align:center;">${title}</H1><H5 style="color:#FF3333; text-align:center;">${message}</H5>`);
+}
+allTabs("/searchCrazy.html", count => { if (count<2) startSearchCrazy(); else {
+  displayDebuggerError(`Error starting SearchCrazy Page.`,`You have SearchCrazy Page running in another tab or window. You can't have multiple instances running or it will cause database problems.`);
+}});
