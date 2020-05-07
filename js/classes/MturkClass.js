6ead5a619cd006f84177dc99a29bf33579bf61e3
John Ramirez <johnnyrs@allbyjohn.com>
2020-05-07 14:56:53 -0700

    Recognizes error when group ID not found

--- js/classes/MturkClass.js
+++ js/classes/MturkClass.js
@@ -19,10 +19,11 @@ class MturkClass {
 			this.addTotalFetched();
 			let returnObj = { type:result.type, status:result.status, mode: "", data:result.data, url:result.url };
 			if (this.resultUrl && this.resultUrl.includes('https://www.amazon.com/ap/signin')) { returnObj.mode = "logged out"; returnObj.data = null; }
-			else if (result.type === "ok.json" && result.data.error && result.data.error === "You have exceeded the allowable page request rate") { this.addPRE(); returnObj.mode = "pre"; returnObj.data = null; }
-			else if (result.type === "ok.json" && result.data.message && result.data.message.includes("You have accepted the maximum number of HITs allowed at one time")) { returnObj.mode = "maxxedOut"; returnObj.data = null; }
+			else if (result.type === "ok.json" && result.data.error && result.data.error.includes("You have exceeded the allowable")) { this.addPRE(); returnObj.mode = "pre"; returnObj.data = null; }
+			else if (result.type === "ok.json" && result.data.message && result.data.message.includes("You have accepted the maximum number of HITs")) { returnObj.mode = "maxxedOut"; returnObj.data = null; }
 			else if (result.type === "ok.json" && result.data.message && result.data.message === "There are no more of these HITs available.") { returnObj.mode = "noMoreHits"; returnObj.data = null; }
 			else if ( result.type === "ok.json" && result.data.message && result.data.message.includes("you do not meet those Qualifications") ) { returnObj.mode = "noQual"; returnObj.data = null; }
+			else if ( result.type === "ok.json" && result.data.message && result.data.message.includes("address is not valid") ) { returnObj.mode = "notValid"; returnObj.data = null; }
 			else if ( result.type === "ok.json" && result.data.message && result.data.message.includes("prevent you from working") ) { returnObj.mode = "blocked"; returnObj.data = null; }
 			else if ( result.type === "ok.json" && result.data.message ) { returnObj.mode = "unknown";  }
 			return returnObj;
