2d37a50f8ef1a48d77b51c67eccef3a0beb06e59
John Ramirez <johnnyrs@allbyjohn.com>
2020-05-14 06:48:00 -0700

    Tabs now saved

--- js/classes/TabbedClass.js
+++ js/classes/TabbedClass.js
@@ -7,18 +7,31 @@ class TabbedClass {
     this.ulId = ulId;
     this.contentId = contentId;
     this.tabIds = "pcm-t";
-    this.addButton = false;
-    this.renameTab = false;
-    this.deleteTab = false;
-    this.draggable = false;
-
+    this.addButton = false;           // Can users add tabs by using an add button?
+    this.renameTab = false;           // Can users rename a tab?
+    this.deleteTab = false;           // Can users delete a tab?
+    this.draggable = false;           // Are objects in tabs draggable?
+    this.dataTabs = [{title:"Main", position:0, list:[]}, {title:"Daily", position:1, list:[]}];
     this.tabStructure(this.attachedTo, divId, ulId, contentId);
-    if (ulId==="pcm_tabbedPandas") {
-      this.addAddButton($(`#${ulId}`));
-      this.renameTab = true; this.deleteTab = true; this.draggable = true;
-      this.addTab("Main", true);
-      this.addTab("Test");
-    } else $(`<li class="pcm_captchaText ml-2"></li>`).appendTo($(`#${ulId}`));
+  }
+  getAllFromDB(afterFunc) {
+    bgPanda.db.getFromDB(bgPanda.tabsStore, "cursor", null, (cursor) => { return cursor.value; })
+      .then( (result) => {
+        if (result.length) { this.dataTabs = result; this.prepareAddTabs(true, afterFunc);
+        } else this.prepareAddTabs(false, afterFunc);
+      })
+  }
+  prepareAddTabs(fromDB, afterFunc) {
+    let active = true;
+    this.dataTabs.forEach( theTab => { this.addTab(theTab.title, active, !fromDB); active = false; });
+    afterFunc.apply(this);
+  }
+  prepare(afterFunc) {
+    if (this.ulId==="pcm_tabbedPandas") { // Is this tab row for the panda's?
+      this.addAddButton($(`#${this.ulId}`)); // Shows the add tab button on the tabbed panda row
+      this.renameTab = true; this.deleteTab = true; this.draggable = true; // Allows renaming, deleting and dragging
+      this.getAllFromDB(afterFunc);
+    } else $(`<li class="pcm_captchaText ml-2"></li>`).appendTo($(`#${this.ulId}`));
   }
   tabStructure(element, divId, ulId, contentId) {
     $(`<div id="${divId}" class="h-100 p-0"></div>`).append($(`<ul class="nav nav-tabs" id="${ulId}" role="tablist"></ul>`)).append($(`<div id="${contentId}" class="tab-content"></div>`)).appendTo($(element));
@@ -29,37 +42,44 @@ class TabbedClass {
       e.preventDefault();
       const label = prompt(`Enter New Tab Label: `, `Added-${this.unique}`);
       if (label && returnName!=="") {
-        this.addTab(label);
-        console.log(this.tabObject);
+        this.addTab(label, false, true);
       }
     } )).appendTo($(tabElement));
   }
-  addTab(name, active=false) {
+  addTab(name, active=false, addToDB=false) {
     const unique = this.unique++;
+    if (addToDB) bgPanda.db.addToDB(bgPanda.tabsStore, {title:name, position:unique, list:[]})
+      .then( () => {} );
     this.tabObject[unique] = {label:name};
     const activeText = (active) ? " active" : "";
     const start = $(`<li class="nav-item"></li>`).data("unique", unique);
     if (this.addButton) $(start).insertBefore($(`#${this.ulId}`).find(`.pcm_addTab`))
       .droppable( {tolerance:'pointer', over: (e, ui) => {
-          const unique = $(e.target).data("unique");
-          const tabUnique = $(ui.draggable).data("pandaObj").tabUnique;
           $(e.target).find("a").click();
         },
         drop: (e, ui) => {
-          const unique = $(e.target).data("unique");
-          const pandaObj = $(ui.draggable).data("pandaObj");
-          if (unique!==pandaObj.tabUnique) {
-            setTimeout( () => { 
-              ui.draggable.detach().appendTo($(`#pcm-t${unique}Content .card-deck:first`));
-              pandaObj.tabUnique = unique;
-            },0);
+          function changeTab(e, hitInfo, myId) { console.log("droppable - " + hitInfo.id);
+            const unique = $(e.target).data("unique");
+            if (unique!==hitInfo.tabUnique) {
+              setTimeout( () => { 
+                ui.draggable.detach().appendTo($(`#pcm-t${unique}Content .card-deck:first`));
+                hitInfo.tabUnique = unique;
+                bgPanda.updateDbData(myId, hitInfo);
+              },0);
+            }
+          }
+          const myId = $(ui.draggable).data("myId");
+          // if database data loaded then change tabunique to new tab number and update
+          if (bgPanda.info[myId].data) changeTab(e, bgPanda.info[myId].data, myId);
+          else { // load up data from database and then change tabunique and update
+            bgPanda.getDbData(myId, (r) => { changeTab(e, r, myId); });
           }
         }
       });
     else $(start).insertBefore($(`#${this.ulId}`).find(`.pcm_captchaText`));
     const label = $(`<a class="nav-link${activeText} small py-0 px-2" id="${this.tabIds}${unique}Tab" data-toggle="tab" href="#${this.tabIds}${unique}Content" role="tab" aria-controls="${this.tabIds}${unique}Content" aria-selected="${(active) ? "true" : "false"}"></a>`).appendTo(start);
     if (this.renameTab) $(label).bind('contextmenu', (e) => {
-        if ($(e.target).closest("li").data("unique")!==0) {
+        if ($(e.target).closest("li").data("unique")!==0) { // First tab can not be renamed ever.
           const label = prompt(`Enter New Tab Label: `, $(e.target).text());
           if (label && label!=="") { this.tabObject[unique] = {label:label}; $(e.target).html(label); }
           e.preventDefault();
@@ -77,12 +97,20 @@ class TabbedClass {
     const tabPane = $(`<div class="tab-pane pcm_tabs p-0 show${activeText}" id="${this.tabIds}${unique}Content" name="${name}" role="tabpanel"></div>`).appendTo(`#${this.contentId}`);
     if (this.draggable) $(tabPane).append($(`<div class="card-deck p-0 px-1"></div>`).data("unique",unique).sortable({ opacity:0.5, cursor:"move", appendTo: document.body, helper: "clone",
         stop: function(e, ui) {
-          const unique = $(`#pcm_tabbedPandas a.active:first`).closest("li").data("unique");
-          const pandaObj = $(ui.item).closest(".card").data("pandaObj");
-          const tabUnique = pandaObj.tabUnique;
-          if (unique!==tabUnique) {
-            ui.item.detach().appendTo($(`#pcm-t${unique}Content .card-deck:first`));
-            pandaObj.tabUnique = unique;
+          function changePos(e, hitInfo, myId) { console.log("sortable - " + hitInfo.id);
+            const unique = $(`#pcm_tabbedPandas a.active:first`).closest("li").data("unique");
+            if (unique===hitInfo.tabUnique) {
+              // ui.item.detach().appendTo($(`#pcm-t${unique}Content .card-deck:first`));
+              // hitInfo.tabUnique = unique;
+              // bgPanda.updateDbData(myId, hitInfo);
+            }
+          }
+          const myId = $(ui.item).data("myId");
+          // if database data loaded then change tabunique to new tab number and update
+          if (bgPanda.info[myId].data) changePos(e, bgPanda.info[myId].data, myId);
+          else { // load up data from database and then change tabunique and update
+            bgPanda.getDbData(myId, (r) => { changePos(e, r, myId);
+            });
           }
         }
     }));
